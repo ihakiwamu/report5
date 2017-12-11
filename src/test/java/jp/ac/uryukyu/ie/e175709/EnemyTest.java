3049c3f0089d66b323d2bc81728063fa04c92de8
Kiwamu Iha <e175709@ie.u-ryukyu.ac.jp>
2017-12-11 19:37:14 +0900

    レポート課題５,Step1

---
+++ src/test/java/jp/ac/uryukyu/ie/e175709/EnemyTest.java
@@ -0,0 +1,19 @@
+package jp.ac.uryukyu.ie.e175709;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class EnemyTest {
+    @Test
+    public void attack() {
+        int heroHP = 10;
+        Hero hero = new Hero("テスト勇者", heroHP, 5);
+        Enemy enemy = new Enemy("テストスライム", 6, 3);
+        enemy.setDead(true);
+        for (int i = 0; i < 10; i++) {
+            enemy.attack(hero);
+        }
+        assertEquals(heroHP, hero.getHitPoint());
+    }
+}
